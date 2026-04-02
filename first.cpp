de059ee04fe2e76cb418a8bc8b4100d181dc16aa
Prem <premkumar33652@gmail.com>
2026-04-02 23:43:13 +0530

    Starting c++

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace ::std;
+
+
+int main(){
+    string a ;
+    string b ;
+    cout<<"Enter your name ";
+    cin>>a;
+    cout<<"Enter  your Class";
+    cin>>b;
+    cout<<"your name is : "<<a<<"\n and you are in : "<<b;
+}
