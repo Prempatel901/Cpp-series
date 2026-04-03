8646ba7eb67395b7a440603c02a5558e97ae0306
Prem <premkumar33652@gmail.com>
2026-04-03 23:24:23 +0530

    5 simple question

---
+++ dsa.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace ::std ;
+
+
+
+
+int main(){
+
+//  2 numbers input lo aur bada number print karo---------------------👍
+
+// int a,b,c;
+// cout<<"Enter two number: ";
+// cin>>a>>b;
+// if(a>b)
+//     cout<<a;
+// else
+//     cout<<b;
+
+// second method-----------------------
+
+// int a,b,c;
+// cout<<"Enter two number: ";
+// cin>>a>>b;
+
+// c= a>b?a:b;
+// cout<< c;
+
+
+
+//  Ek number lo aur check karo positive / negative / zero-----------------👍
+
+// int a ;
+
+// cout<<"Enter a number: ";
+// cin>>a;
+
+// if(0<a)
+//     cout<<"Positive";
+// else if(0>a)
+//     cout<<"Negative";
+// else 
+//     cout<<"Zero";
+
+
+
+//  Ek number ka table print karo ------------------------👍
+
+// int a ;
+// cout<<"Enter a number:  ";
+// cin>>a;
+// for(int i = 1; i<=10; i++){
+//     cout<<a<<" * "<<i<<" = "<<i*a<<"\n";
+// }
+
+
+
+
+//  1 se N tak sum nikaalo----------------- 👍
+
+// int n,sum= 0;
+// cout<<"Enter a number: ";
+// cin>>n;
+
+// for(int i = 1; i<=n; i++)
+// {
+//     sum = sum+i;
+// }
+
+// cout<<"total sum is : "<<sum;
+
+
+//  1 se N tak even numbers print karo----------👍
+
+// int n;
+// cout<<"Enter N number: ";
+// cin>>n;
+// for(int i=1;i<=n;i++){
+//     if(i%2);
+//     else
+//         cout<<i<<'\n';
+        
+// }
+
+}
+
+
