3b507f0e15a78e943c1da57f3f163d2c83205ef9
Prem <premkumar33652@gmail.com>
2026-04-04 23:57:39 +0530

    simple ques

---
+++ dsa2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace ::std;
+
+
+int main(){
+
+// Ek number ka factorial nikaalo
+
+// int a, fact=1;
+// cout<<"Enter a number: ";
+// cin>>a;
+// for(int i=1; i<=a; i++){
+//     fact = fact*i;
+    
+// }
+//  cout<<fact;
+//  return 0;
+
+
+}
