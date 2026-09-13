adc1c85106661f1ac8195d118f2e746e403c5509
Prem Patel <premkumar33652@gmail.com>
2026-09-13 07:07:10 +0530

    updated

--- dsa01.cpp
+++ dsa01.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
-#define ARRAY_OVERFLOW
+#define ARRAY_OVERFLOW 1
+#define INVALID_INDEX 2
+
 
 class Array
 {
@@ -13,6 +15,9 @@ class Array
         bool isEmpty();
         bool isFull();
         void append(int);
+        void insert(int,int);
+        void edit(int,int);
+        void del(int);
 };
 
 Array::Array(int cap)
@@ -37,4 +42,34 @@ void Array::append(int data){
         throw ARRAY_OVERFLOW;
     ptr[lastIndex+1]=data;
     lastIndex++;
+}
+
+void Array::insert(int index,int data){
+    if(index<0 || index>lastIndex+1)
+        throw INVALID_INDEX;
+    if(isFull())
+        throw  ARRAY_OVERFLOW;
+    int i;
+    for(i=lastIndex;i>=index;i--)
+    {
+        ptr[i+1]=ptr[i];
+    }
+    ptr[index]=data;
+    lastIndex++;
+}
+
+
+void Array::edit(int index,int data){
+    if(index<0||index>lastIndex)
+        throw INVALID_INDEX;
+    ptr[index]=data;
+}
+
+void Array::del(int index){
+    if(index<0 || index>lastIndex)
+        throw INVALID_INDEX;
+    int i;
+    for(i=index;i<lastIndex;i++){
+        ptr[i]=ptr[i+i];
+    }
 }
